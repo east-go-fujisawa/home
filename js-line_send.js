3ace4972fc5c2b1c92096406fd786e136765b819
east-go-fujisawa <matsuo-kazuya1@ed.tmu.ac.jp>
2025-08-19 08:25:34 +0000

    No.2

--- js-line_send.js
+++ js-line_send.js
@@ -21,12 +21,10 @@ var datas = [{"1":[{"name":[],"id":[]}],"2":[{"name":[],"id":[]}],"3":[{"name":[
 for(var i = 1; i<5; i+=2){
 var last = sheet.getActiveSheet().getRange(1,i).getNextDataCell(SpreadsheetApp.Direction.DOWN).getLastRow();
 var names = sheet.getActiveSheet().getRange(2,i,last,1).getValues();
-var ids = sheet.getActiveSheet().getRange(2,i+1,last,1).getValues();
-var count = -1;
+//var ids = sheet.getActiveSheet().getRange(2,i+1,last,1).getValues();
 for(var n of names){
-count++;
 datas[0][i][0]["name"].push(n);
-datas[0][i][0]["id"].push(ids[count]);
+//datas[0][i][0]["id"].push(ids[count]);
 }
 }
 */
@@ -44,4 +42,30 @@ function change(){
         document.getElementById("names2").insertAdjacentHTML("beforeend",text2);
     }
 }
+var add_name = [];
+function add(text){
+        if(names_ids_datas.length > 0){
+        document.getElementById(text).remove();
+        var text2 = '<p class="b1" id="'+n+'"onclick=del('+n+')>'+n+'</p>';
+        document.getElementById("add_names").insertAdjacentHTML("beforeend",text2);
+        add_name.push(text);
+        }else{
+            alert("データ取得中です\nしばらくお待ちください\n2秒後自動で処理を行います");
+            setTimeout(()=>{
+                add(text);
+            },2000);
+        }
+}
 
+function del(text){
+        document.getElementById(text).remove();
+        var text2 = '<p class="b1" id="'+n+'"onclick=add('+n+')>'+n+'</p>';
+        document.getElementById("names2").insertAdjacentHTML("beforeend",text2);
+        var count = -1;
+        for(var n of add_name){
+            count++;
+            if(n == text){
+                add_name = add_name.splice(count,1);//若干怪しい定義を確認した方が良き
+            }
+        }
+}
